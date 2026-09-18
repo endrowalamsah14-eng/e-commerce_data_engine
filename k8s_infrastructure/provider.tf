9f4277127222e0ea8802d4c6ed7388ade5f3411f
endrowalamsah14-eng <endrowalamsah14@gmail.com>
2026-09-18 20:59:50 +0700

    Add Terraform provider configuration for Hetzner Cloud

---
+++ k8s_infrastructure/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~> 1.45"
+    }
+  }
+}
+
+variable "hcloud_token" {
+  type      = string
+  sensitive = true
+}
+
+provider "hcloud" {
+  token = var.hcloud_token
+}
